d24961ff50ec71fa4d9fb38c0fd5cdc9ac32fde6
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-09 23:53:45 +0900

    feat: add Snake struct and implement snake drawing

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
+use std::collections::LinkedList;
+
 use piston_window::*;
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Snake Game", [500; 2])
+    let mut window: PistonWindow = WindowSettings::new("Snake Game", [750; 2])
         .exit_on_esc(true)
         .build()
         .unwrap();
 
     // green of snake
     let snake_color = [0.0, 0.5, 0.3, 1.0];
+    let mut snake = Snake::new(snake_color);
 
-    let mut x = 50.0;
-    let mut y = 50.0;
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _| {
             clear([0.5, 0.5, 0.5, 1.0], g);
-            rectangle(
-                snake_color,
-                [x, y, 50.0, 50.0], // rectangle
-                c.transform,
-                g,
-            );
+            snake.draw_snake(&c, g);
         });
 
-        if let Event::Input(Input::Button(key), _) = e {
+        /* if let Event::Input(Input::Button(key), _) = e {
             match key.button {
                 Button::Keyboard(Key::W) => y -= 50.0,
                 Button::Keyboard(Key::A) => x -= 50.0,
@@ -30,6 +26,37 @@ fn main() {
                 Button::Keyboard(Key::S) => y += 50.0,
                 _ => (),
             }
+        } */
+    }
+}
+
+struct Snake {
+    color: [f32; 4],
+    body: LinkedList<(f64, f64)>,
+    tail: (f64, f64),
+}
+
+impl Snake {
+    pub fn new(color: [f32; 4]) -> Self {
+        let mut body = LinkedList::new();
+        body.push_back((150.0, 50.0));
+        body.push_back((100.0, 50.0));
+        body.push_back((50.0, 50.0));
+        Self {
+            color,
+            body: body,
+            tail: (50.0, 50.0),
         }
+    }
+
+    fn draw_snake(&self, c: &Context, g: &mut G2d) {
+        self.body.iter().for_each(|(x, y)| {
+            rectangle(
+                self.color,
+                [*x, *y, 50.0, 50.0], // rectangle
+                c.transform,
+                g,
+            )
+        });
     }
 }
